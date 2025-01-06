63a5fb97259c7fc4811a6f010b21f232688f8d58
s20488 <s20488@pjwstk.edu.pl>
2025-01-06 21:00:21 +0100

    fix

--- engine_finetune.py
+++ engine_finetune.py
@@ -15,7 +15,7 @@ from timm.utils import accuracy
 from typing import Iterable, Optional
 import util.misc as misc
 import util.lr_sched as lr_sched
-from sklearn.metrics import roc_auc_score, average_precision_score, multilabel_confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import roc_auc_score, average_precision_score,multilabel_confusion_matrix
 from pycm import *
 import matplotlib.pyplot as plt
 import numpy as np
@@ -210,9 +210,9 @@ def evaluate(data_loader, model, device, task, epoch, mode, num_class):
         true_label_onehot_array = np.array(true_label_onehot_list)
         prediction_array = np.array(prediction_list)
 
-        cm = confusion_matrix(true_label_decode_list, prediction_decode_list)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
-        disp.plot(cmap=plt.cm.Blues, values_format=".2f")
+        cm = ConfusionMatrix(actual_vector=true_label_decode_list, predict_vector=prediction_decode_list)
+        cm.labels = class_names
+        cm.plot(cmap=plt.cm.Blues, number_label=True, normalized=True, plot_lib="matplotlib")
         plt.savefig(task + 'confusion_matrix_test.jpg', dpi=600, bbox_inches='tight')
 
         # Calculate metrics per class
