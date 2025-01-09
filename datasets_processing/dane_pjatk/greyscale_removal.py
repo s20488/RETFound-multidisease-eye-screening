06fa40b83300c291b6a20c0502a79e3db03f5d19
s20488 <s20488@pjwstk.edu.pl>
2025-01-10 00:01:13 +0100

    fix

--- datasets_processing/dane_pjatk/greyscale_removal.py
+++ datasets_processing/dane_pjatk/greyscale_removal.py
@@ -1,31 +1,34 @@
 import os
-from PIL import Image
+import pydicom
 from concurrent.futures import ThreadPoolExecutor
 
 
-def is_grayscale(image_path):
-    image = Image.open(image_path).convert("RGB")
-    pixels = list(image.getdata())
-    for pixel in pixels:
-        if pixel[0] != pixel[1] or pixel[1] != pixel[2]:
-            return False
-    return True
-
-
-def process_image(file_path):
+def is_grayscale_dicom(dicom_path):
     try:
-        if is_grayscale(file_path):
+        ds = pydicom.dcmread(dicom_path)
+        photometric_interpretation = ds.get("PhotometricInterpretation", None)
+        if photometric_interpretation in ["MONOCHROME1", "MONOCHROME2"]:
+            return True
+        return False
+    except Exception as e:
+        print(f"Error reading DICOM file {dicom_path}: {e}")
+        return False
+
+
+def process_dicom(file_path):
+    try:
+        if is_grayscale_dicom(file_path):
             os.remove(file_path)
-            print(f"Deleted black and white image: {file_path}")
+            print(f"Deleted black and white DICOM image: {file_path}")
     except Exception as e:
         print(f"Error in processing of {file_path}: {e}")
 
 
-def remove_grayscale_images_parallel(directory):
+def remove_grayscale_dicom_parallel(directory):
     with ThreadPoolExecutor() as executor:
-        file_paths = [os.path.join(directory, f) for f in os.listdir(directory)]
-        executor.map(process_image, file_paths)
+        file_paths = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(".dcm")]
+        executor.map(process_dicom, file_paths)
 
 
 image_directory = "/mnt/data/cfi"
-remove_grayscale_images_parallel(image_directory)
+remove_grayscale_dicom_parallel(image_directory)
