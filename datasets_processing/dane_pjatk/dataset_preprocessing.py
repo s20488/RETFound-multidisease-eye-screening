f25c4ba724509e63c44df985e1dbb2952b37d5a8
s20488 <s20488@pjwstk.edu.pl>
2025-01-09 23:01:50 +0100

    fix

--- datasets_processing/dane_pjatk/dataset_preprocessing.py
+++ datasets_processing/dane_pjatk/dataset_preprocessing.py
@@ -5,6 +5,8 @@ base_dir = "/mnt/data/cfi"
 
 image_extensions = {".dcm"}
 
+empty_dirs = []
+
 for root, dirs, files in os.walk(base_dir):
     for file in files:
         if any(file.lower().endswith(ext) for ext in image_extensions):
@@ -17,3 +19,13 @@ for root, dirs, files in os.walk(base_dir):
                 print(f"Moved: {file_path} -> {target_path}")
             except Exception as e:
                 print(f"Error moving {file_path}: {e}")
+
+    if not os.listdir(root):
+        empty_dirs.append(root)
+
+for directory in empty_dirs:
+    try:
+        os.rmdir(directory)
+        print(f"Deleted empty directory: {directory}")
+    except Exception as e:
+        print(f"Error deleting directory {directory}: {e}")
