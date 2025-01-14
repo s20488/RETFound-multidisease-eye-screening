5dde3f1ae14f0ba1aea64b9576606f13d9dcda2b
s20488 <s20488@pjwstk.edu.pl>
2025-01-14 19:45:11 +0100

    fix

--- main_finetune.py
+++ main_finetune.py
@@ -322,9 +322,9 @@ def main(args):
         criterion = LabelSmoothingCrossEntropy(smoothing=args.smoothing)
     else:
         #class_weights = torch.tensor([7626 / (5229 + 7626), 5229 / (5229 + 7626)], device=device)
-        #class_weights = torch.tensor([0.2346, 0.7654], device=device)
-        #criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
-        criterion = torch.nn.CrossEntropyLoss()
+        class_weights = torch.tensor([2.026, 1.975], device=device)
+        criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
+        #criterion = torch.nn.CrossEntropyLoss()
 
     print("criterion = %s" % str(criterion))
 
