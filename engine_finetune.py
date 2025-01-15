fa8c4f70e9598d80cf6f77a0ec1edfbc2c829d65
s20488 <s20488@pjwstk.edu.pl>
2025-01-15 22:27:09 +0100

    fix

--- engine_finetune.py
+++ engine_finetune.py
@@ -141,8 +141,9 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
 
 @torch.no_grad()
 def evaluate(data_loader, model, device, task, epoch, mode, num_class):
-    class_weights = torch.tensor([1.237, 5.216], device=device)
-    #class_weights = torch.tensor([2.628, 8.852, 2.522, 9.094], device=device)
+    class_weights = torch.tensor([0.0719, 0.9281], device=device)  # diabetes
+    #class_weights = torch.tensor([1.237, 5.216], device=device)  # hypertansion_strict2
+    #class_weights = torch.tensor([2.628, 8.852, 2.522, 9.094], device=device)  # hypertansion
     criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
     #criterion = torch.nn.CrossEntropyLoss()
 
--- main_finetune.py
+++ main_finetune.py
@@ -322,7 +322,8 @@ def main(args):
         criterion = LabelSmoothingCrossEntropy(smoothing=args.smoothing)
     else:
         #class_weights = torch.tensor([2.628, 8.852, 2.522, 9.094], device=device)
-        class_weights = torch.tensor([1.237, 5.216], device=device)
+        class_weights = torch.tensor([0.0719, 0.9281], device=device)
+        #class_weights = torch.tensor([1.237, 5.216], device=device)
         criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
         #criterion = torch.nn.CrossEntropyLoss()
 
