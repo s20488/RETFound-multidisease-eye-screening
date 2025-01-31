631753692e755c9496d4bd154dc74d662daf5e81
s20488 <s20488@pjwstk.edu.pl>
2025-01-31 19:14:01 +0100

    fix

--- test.py
+++
@@ -1,6 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-
-labels = ['cataract', 'normal']
-encoder = LabelEncoder()
-encoded_labels = encoder.fit_transform(labels)
-print(encoded_labels)  # [0, 1]
