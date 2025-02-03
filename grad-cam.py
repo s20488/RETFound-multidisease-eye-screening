c1cf1b5d5d9a276484fc83cf3107abb0e304cb07
s20488 <s20488@pjwstk.edu.pl>
2025-02-03 18:39:45 +0100

    fix

--- grad-cam.py
+++ grad-cam.py
@@ -103,7 +103,7 @@ def load_and_preprocess_image(image_path):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),  # Приводим к размеру, который принимает модель
         transforms.ToTensor(),  # Преобразуем в тензор
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Нормализация
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Нормализация
     ])
 
     # Применяем преобразования
