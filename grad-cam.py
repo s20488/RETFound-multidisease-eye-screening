dc9d84acb386ee27e11911f4939a227e355ad05f
s20488 <s20488@pjwstk.edu.pl>
2025-02-03 19:03:59 +0100

    fix

--- grad-cam.py
+++ grad-cam.py
@@ -107,7 +107,7 @@ def load_and_preprocess_image(image_path):
     return transform(image).unsqueeze(0)
 
 # Пути к изображениям
-image_path = "/mnt/data/cfi_manual_glaucoma/train/glaucoma/17255_20240925124038874.png"
+image_path = "/mnt/data/cfi_manual_cataract/train/cataract/12594_20220110111209652.png"
 img_tensor = load_and_preprocess_image(image_path)
 
 # Предсказание класса
