81989c5f73f2dc06fd8e2af5f861b95127e9ed6b
s20488 <s20488@pjwstk.edu.pl>
2025-02-26 21:25:28 +0100

    fix

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 # Загружаем данные
-eval_true_labels = np.load("path/to/task/eval_true_labels.npy")
-eval_predicted_probs = np.load("path/to/task/eval_predicted_probs.npy")
-plot_true_labels = np.load("path/to/task/plot_true_labels.npy")
-plot_predicted_probs = np.load("path/to/task/plot_predicted_probs.npy")
+eval_true_labels = np.load("/mnt/data/Anastasiia_Ponkratova/RETFound_MAE/results/finetune_cfi_manual_hypertension_AHA_pr/eval_true_labels.npy")
+eval_predicted_probs = np.load("/mnt/data/Anastasiia_Ponkratova/RETFound_MAE/results/finetune_cfi_manual_hypertension_AHA_pr/eval_predicted_probs.npy")
+plot_true_labels = np.load("/mnt/data/Anastasiia_Ponkratova/RETFound_MAE/results/finetune_cfi_manual_hypertension_AHA_pr/plot_true_labels.npy")
+plot_predicted_probs = np.load("/mnt/data/Anastasiia_Ponkratova/RETFound_MAE/results/finetune_cfi_manual_hypertension_AHA_pr/plot_predicted_probs.npy")
 
 # Сравниваем true_labels
 print("True labels match:", np.array_equal(eval_true_labels, plot_true_labels))
